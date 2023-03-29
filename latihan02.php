2d770581e4c805ac43b9ec28422649f41fa87027
cloud <cloud@34.124.223.51>
2023-03-29 09:43:01 +0800

    first commit

---
+++ latihan02.php
@@ -0,0 +1,11 @@
+<?php
+    $angka1 = 9;
+    $angka2 = 9;
+
+    if ($angka1 == $angka2){
+        //bagian true
+        echo "Nilai Variabel angka1 sama dengan variabel angka2";
+    }else{
+        //bagian false
+        echo " Nilai variabel angka1 tidak sama dengan variabel angka2";
+    }
---
+++ latihan04.php
@@ -0,0 +1,10 @@
+<?php
+    $nAwal = 0;
+    $nAkhir = 11;
+
+    while($nAwal < $nAkhir){
+        //blok yang diulang
+        echo "🎭Perulangan🎭 ke-".$nAwal. "<br>";
+        
+        $nAwal++;
+    }
